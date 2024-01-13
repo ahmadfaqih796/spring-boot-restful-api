2f50ccffebe0e93401561b7360b1b85b33e38d4e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-14 00:13:24 +0700

    join column uesrname in table user to contact

--- src/main/java/com/faqih/restful/entity/Contact.java
+++ src/main/java/com/faqih/restful/entity/Contact.java
@@ -1,11 +1,10 @@
 package com.faqih.restful.entity;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,18 +16,20 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "contacts")
 
-public class User {
+public class Contact {
    @Id
    private Integer id;
-   private String name;
    private String username;
-   private String password;
-   private String token;
-   @Column(name = "token_expired_at")
-   private Long tokenExpiredAt;
+   @Column(name = "first_name")
+   private Long firstName;
+   @Column(name = "last_name")
+   private Long lastName;
+   private String phone;
+   private String email;
 
-   @OneToMany(mappedBy = "user")
-   private List<Contact> contacts;
+   @ManyToOne
+   @JoinColumn(name = "username", referencedColumnName = "username")
+   private User user;
 }
