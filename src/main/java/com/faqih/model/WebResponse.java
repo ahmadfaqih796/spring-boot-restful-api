e3ba17c6ad99e6252c655e16ce692766a34255f1
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-14 00:46:56 +0700

    register request

--- src/main/java/com/faqih/model/WebResponse.java
+++ src/main/java/com/faqih/model/WebResponse.java
@@ -1,5 +1,17 @@
 package com.faqih.model;
 
-public class WebResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+// T itu tanda sebagai generic class
+public class WebResponse<T> {
+   private T data;
+   private String message;
+   private String errors;
 }
